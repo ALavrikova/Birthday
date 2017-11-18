e7eb14e85a79860d8db1e1654188f42db79bed95
Anton <anton@andreys-MacBook-Pro.local>
2017-11-18 08:43:17 -0800

    initial commit

--- HappyBirthday/ViewController.swift
+++ HappyBirthday/ViewController.swift
@@ -2,24 +2,45 @@
 //  ViewController.swift
 //  HappyBirthday
 //
-//  Created by Anton on 11/16/17.
-//  Copyright © 2017 Anton. All rights reserved.
+//  Created on 11/16/17.
+//  Copyright © 2017. All rights reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var bombSoundEffect: AVAudioPlayer?
+    
+    @IBOutlet weak var wishLabel: UILabel!
+    var firstName: String?
+    var lastName: String?
+    var age = 30
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        firstName = "Vish"
+        lastName = ""
+        age = age + 1
+        let message = "Wishing \(String(describing: firstName!)) \(String(describing: lastName!)) a very Happy " + String(age) +  "st Birthday!"
+        wishLabel.text = message
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named:"bollywood.jpg")!)
+        
+        let path = Bundle.main.path(forResource: "Happy-Birthday-Song-In-Hindi.mp3", ofType:nil)!
+        
+        let url = URL(fileURLWithPath: path)
+        
+        do {
+            bombSoundEffect = try AVAudioPlayer(contentsOf: url)
+            bombSoundEffect?.play()
+        } catch {
+            print("cant play")
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
